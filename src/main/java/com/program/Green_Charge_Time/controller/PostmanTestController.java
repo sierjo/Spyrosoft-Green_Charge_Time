bc2844466c5aa460ae2daec96388f2decd627be9
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2026-06-21 17:59:21 +0300

    feat: add endpoint for energy-types
    - Update controller 'PostmanTestController':
    	- Add 'getDailyMix' method mapped to '/energy-types' to return mocked daily statistics.
    	- Populate 'DailyStatisticsResponse' with hardcoded test data including a specific date, an 'averageMix' map.

--- src/main/java/com/program/Green_Charge_Time/controller/PostmanTestController.java
+++ src/main/java/com/program/Green_Charge_Time/controller/PostmanTestController.java
@@ -1,10 +1,13 @@
 package com.program.Green_Charge_Time.controller;
 
+import com.program.Green_Charge_Time.dto.DailyStatisticsResponse;
 import com.program.Green_Charge_Time.dto.OptimalTimeResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -17,6 +20,22 @@ public class PostmanTestController {
         response.setTimeStastChargin("2026-06-21-12:00");
         response.setTimeEndChargin("2026-06-21-18:00");
         response.setCleanEnergyPercentage("85.5");
+        return response;
+    }
+
+    @GetMapping("/energy-types")
+    public DailyStatisticsResponse getDailyMix() {
+        DailyStatisticsResponse response = new DailyStatisticsResponse();
+        response.setDate("2026-06-21");
+
+        Map<String, Double> mix = new HashMap<>();
+        mix.put("wind", 45.5);
+        mix.put("solar", 10.0);
+        mix.put("gas", 20.0);
+
+        response.setAverageMix(mix);
+        response.setCleanEnergyPercentage(55.5); // wind + solar
+
         return response;
     }
 }
