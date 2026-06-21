c61b810a199158e428cb7057fb0ebdabf4987741
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2026-06-21 22:01:03 +0300

    feat: implement automated 3-day energy statistics retrieval
    - Update service EnergyService:
    	- Add 'getThreeDaysStatistics' method to dynamically calculate date ranges using 'ZonedDateTime'.
    	- Iterate API calls to fetch generation data for each of the calculated days.
    	- Add 'calculateDailyAverage' to average calculate.
    	- Removed 'fetchRawEnergyData' method.
    - Update controller 'EnergyController':
    	- Change endpoint from '/raw' to a parameterless '/daily-Statistics' mapping.
    	- Refactor to call 'getThreeDaysStatistics' and return a 'List<DailyStatisticsResponse>'.

--- src/main/java/com/program/Green_Charge_Time/controller/EnergyController.java
+++ src/main/java/com/program/Green_Charge_Time/controller/EnergyController.java
@@ -1,11 +1,14 @@
 package com.program.Green_Charge_Time.controller;
 
 import com.program.Green_Charge_Time.dto.ApiResponse;
+import com.program.Green_Charge_Time.dto.DailyStatisticsResponse;
 import com.program.Green_Charge_Time.service.EnergyService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/energy")
@@ -18,10 +21,8 @@ public class EnergyController {
     }
 
 
-    @GetMapping("/raw")
-    public ApiResponse getRawData(
-            @RequestParam String from,
-            @RequestParam String to) {
-        return energyService.fetchRawEnergyData(from, to);
+    @GetMapping("/daily-Statistics")
+    public List<DailyStatisticsResponse> getDailyStatistics() {
+        return energyService.getThreeDaysStatistics();
     }
 }
--- src/main/java/com/program/Green_Charge_Time/service/EnergyService.java
+++ src/main/java/com/program/Green_Charge_Time/service/EnergyService.java
@@ -1,21 +1,55 @@
 package com.program.Green_Charge_Time.service;
 
 import com.program.Green_Charge_Time.dto.ApiResponse;
+import com.program.Green_Charge_Time.dto.DailyStatisticsResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class EnergyService {
     private final RestTemplate restTemplate;
     private static final String API_URL = "https://api.carbonintensity.org.uk/generation/{from}/{to}";
-
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm'Z'");
 
     public EnergyService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
-    public ApiResponse fetchRawEnergyData(String from, String to) {
 
-        return restTemplate.getForObject(API_URL, ApiResponse.class, from, to);
+    public List<DailyStatisticsResponse> getThreeDaysStatistics() {
+        List<DailyStatisticsResponse> threeDaysStats = new ArrayList<>();
+
+        // Today
+        ZonedDateTime todayStart = ZonedDateTime.now(ZoneOffset.UTC).truncatedTo(ChronoUnit.DAYS);
+
+        // for three days
+        for (int i = 0; i < 3; i++) {
+            ZonedDateTime startOfDay = todayStart.plusDays(i);
+            ZonedDateTime endOfDay = startOfDay.plusDays(1);
+
+            String from = startOfDay.format(DATE_TIME_FORMATTER);
+            String to = endOfDay.format(DATE_TIME_FORMATTER);
+
+            ApiResponse rawData = restTemplate.getForObject(API_URL, ApiResponse.class, from, to);
+
+            DailyStatisticsResponse dailyStat = calculateDailyAverage(rawData,startOfDay.toLocalDate().toString());
+            threeDaysStats.add(dailyStat);
+        }
+
+        return threeDaysStats;
+    }
+
+    private DailyStatisticsResponse calculateDailyAverage(ApiResponse apiResponse, String date) {
+        DailyStatisticsResponse response = new DailyStatisticsResponse();
+        response.setDate(date);
+
+        return response;
     }
 }
